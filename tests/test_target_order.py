37c3f7a0e5f08fa71e95aacfcc619e3b312b71f9
Aaron McGeever <aaron.mcgeever@gmail.com>
2018-10-04 15:24:44 -0700

    Updated tests to use real targets.
    Used real gene with real targets.

--- tests/test_target_order.py
+++ tests/test_target_order.py
@@ -8,5 +8,18 @@ from common import Gene
 def test_targets_are_in_cut_order():
     gene = Gene("test_gene_with_targets")
     gene.load_targets("dna_good_5_9_18.txt")
-    
-    assert [t.cut for t in gene.targets] == [25, 35]
+    expected_target_cuts = [
+        21,
+        39,
+        40,
+        74,
+        170,
+        201,
+        208,
+        208,
+        209,
+        210,
+        211
+    ]
+
+    assert [t.cut for t in gene.targets] == expected_target_cuts
